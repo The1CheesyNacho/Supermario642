16a71cb18a18b535fa4ee7714c1582fcc9a26f89
The1CheesyNacho <oliveirawillian72@gmail.com>
2023-10-17 16:59:36 -0300

    first release

---
+++ actors/waluigi/texture.inc.c
@@ -0,0 +1,65 @@
+Gfx waluigi_mario_button_ci8_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_mario_button_ci8[] = {
+	#include "actors/waluigi/mario_button.ci8.inc.c"
+};
+
+Gfx waluigi_mario_button_pal_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_mario_button_pal_rgba16[] = {
+	#include "actors/waluigi/mario_button.rgba16.pal"
+};
+
+Gfx waluigi__0_default_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi__0_default_rgba16[] = {
+	#include "actors/waluigi/_0-default.rgba16.inc.c"
+};
+
+Gfx waluigi_saturn_mouth_default_rgba16_rgba16_ci4_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_saturn_mouth_default_rgba16_rgba16_ci4_rgba16[] = {
+	#include "actors/waluigi/saturn_mouth_default_rgba16_rgba16.ci4.rgba16.inc.c"
+};
+
+Gfx waluigi__04003090_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi__04003090_rgba16[] = {
+	#include "actors/waluigi/_04003090.rgba16.inc.c"
+};
+
+Gfx waluigi__04002090_ci4_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi__04002090_ci4_rgba16[] = {
+	#include "actors/waluigi/_04002090.ci4.rgba16.inc.c"
+};
+
+Gfx waluigi_waluigi_eye_halfclosed_rgba16_ci4_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_waluigi_eye_halfclosed_rgba16_ci4_rgba16[] = {
+	#include "actors/waluigi/waluigi_eye_halfclosed_rgba16.ci4.rgba16.inc.c"
+};
+
+Gfx waluigi_waluigi_eye_closed_rgba16_rgba16_ci4_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_waluigi_eye_closed_rgba16_rgba16_ci4_rgba16[] = {
+	#include "actors/waluigi/waluigi_eye_closed_rgba16_rgba16.ci4.rgba16.inc.c"
+};
+
+Gfx waluigi_saturn_eye_closed_rgba16_rgba16_rgba16_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_saturn_eye_closed_rgba16_rgba16_rgba16_rgba16[] = {
+	#include "actors/waluigi/saturn_eye_closed_rgba16_rgba16.rgba16.inc.c"
+};
+
+Gfx waluigi_waluigi_eye_dead_rgba16_ci4_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_waluigi_eye_dead_rgba16_ci4_rgba16[] = {
+	#include "actors/waluigi/waluigi_eye_dead_rgba16.ci4.rgba16.inc.c"
+};
+
+Gfx waluigi_saturn_leftwing_default_rgba16_ia8_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_saturn_leftwing_default_rgba16_ia8[] = {
+	#include "actors/waluigi/saturn_leftwing_default.rgba16.ia8.inc.c"
+};
+
+Gfx waluigi_saturn_rightwing_default_rgba16_ia8_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi_saturn_rightwing_default_rgba16_ia8[] = {
+	#include "actors/waluigi/saturn_rightwing_default.rgba16.ia8.inc.c"
+};
+
+Gfx waluigi__04004890_rgba16_aligner[] = {gsSPEndDisplayList()};
+u8 waluigi__04004890_rgba16[] = {
+	#include "actors/waluigi/_04004890.rgba16.inc.c"
+};
+
