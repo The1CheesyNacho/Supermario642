134d516955a4ab3a01373dbaa36b3ffa198618d2
The1CheesyNacho <oliveirawillian72@gmail.com>
2023-10-24 13:57:01 -0300

    webi wabo

---
+++ levels/castle_grounds/texscroll.inc.h
@@ -0,0 +1,3 @@
+extern void scroll_castle_grounds_dl_castle_grounds_seg7_dl_0700A290_mesh_001_mesh_layer_5_vtx_0();
+extern void scroll_castle_grounds_dl_Water_Box_Mesh_mesh_layer_5_vtx_0();
+extern void scroll_castle_grounds();
